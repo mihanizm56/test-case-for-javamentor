c9337f2dc8a81a680acdc33a765659b6480a0ccf
mihanizm56 <m727507.56@mail.ru>
2019-07-20 23:20:57 +0400

    fix

--- src/components/atoms/select/index.jsx
+++ src/components/atoms/select/index.jsx
@@ -1,20 +1,25 @@
-import React, { memo } from 'react'
-import SelectMaterial from '@material-ui/core/Select'
+import React, { memo } from "react";
+import SelectMaterial from "@material-ui/core/Select";
+import icon from "../../../assets/icons/scroll-icon.svg";
 
-export const Select = memo(({ handleChange, selectedValue, children }) => console.log('Select selectedValue', selectedValue) || (
-    <SelectMaterial
+export const Select = memo(
+  ({ handleChange, selectedValue, children }) =>
+    console.log("Select icon", icon) || (
+      <SelectMaterial
         fullWidth
         value={selectedValue}
         children={children}
         onChange={handleChange}
         style={{
-            height: '40px',
-            border: '1px solid blue',
-            borderRadius: '10px'
+          height: "40px",
+          padding: "0 5px 0 10px",
+          border: "1px solid blue",
+          borderRadius: "10px"
         }}
         disableUnderline
-        variant='outlined'
-    />
-))
+        variant="outlined"
+      />
+    )
+);
 
-Select.defaultProps = { selectedValue: '' }
+Select.defaultProps = { selectedValue: "" };
