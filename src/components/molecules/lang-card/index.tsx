664329f33d29fa450942fb7b328a4edea2145efc
mihanizm56 <m727507.56@mail.ru>
2019-07-22 14:28:16 +0400

    fix

--- src/components/molecules/lang-card/index.tsx
+++ src/components/molecules/lang-card/index.tsx
@@ -5,26 +5,28 @@ interface LangCardType {
   title: string;
   createdAt: number;
   numberOfProjects: number;
-  link: string;
+  link?: string;
   logo: string;
 }
 
 export const LangCard = memo<LangCardType>(
   ({ title, createdAt, numberOfProjects, link, logo }) => (
     <div className="card-container">
-      <div className="card-first-line">
+      <div className="card-first-block">
         <img alt="logo" src={logo} height="118px" width="118px" />
       </div>
-      <div className="card-second-line">
+      <div className="card-second-block">
         <span className="card__lang-name">{title}</span>
         <span className="card__created">{`Основан в ${createdAt} году`}</span>
         <span className="card__number-of-projects">
           {`Проектов на GitHub ${numberOfProjects}`}
         </span>
-        <a className="card__link" href={link}>
-          Документация
-        </a>
+        {link && (
+          <a className="card__link" href={link}>
+            Документация
+          </a>
+        )}
       </div>
     </div>
-  ),
+  )
 );
--- src/components/molecules/lang-list/index.tsx
+++ src/components/molecules/lang-list/index.tsx
@@ -3,30 +3,28 @@ import { LangCard } from "../lang-card";
 import "./lang-list.css";
 
 export interface LangCardType {
-    name: string;
-    year: number;
-    projectsCount: number;
-    docs: string;
-    logo: string;
+  name: string;
+  year: number;
+  projectsCount: number;
+  docs?: string;
+  logo: string;
 }
 
 interface LangListType {
-    langData: Array<LangCardType>;
+  langData: Array<LangCardType>;
 }
 
 export const LangList = memo<LangListType>(({ langData }) => (
-    <div className="list-container">
-        {
-            langData.map(({name, year, projectsCount, docs, logo}, index) => (
-                <LangCard
-                    title={name}
-                    createdAt={year}
-                    numberOfProjects={projectsCount}
-                    link={docs}
-                    key={index}
-                    logo={logo}
-                />
-            ))
-        }
-    </div>
+  <div className="list-container">
+    {langData.map(({ name, year, projectsCount, docs, logo }, index) => (
+      <LangCard
+        title={name}
+        createdAt={year}
+        numberOfProjects={projectsCount}
+        link={docs}
+        key={index}
+        logo={logo}
+      />
+    ))}
+  </div>
 ));
